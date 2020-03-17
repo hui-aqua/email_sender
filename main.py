52ddd5d60d21d984232d54b034374c594a644060
Hui <50576083+hui-aqua@users.noreply.github.com>
2020-03-17 23:15:59 +0100

    add the host name and last 50 lines to the email

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-email_sender = 'hui.reboot@gmail.com' # Your email
-password_sender = 'Ch19911011' # Your email account password
-email_receiver = 'chenghui.qd@live.cn' # Who you are sending the message to
-
+email_sender = 'hui.reboot@gmail.com'  # Your email
+password_sender = 'Ch19911011'  # Your email account password
+email_receiver = 'chenghui.qd@live.cn'  # Who you are sending the message to
 
 # Message content
-subject = 'Message from gorina1'  # The subject line
-
+host_name = os.uname()[1]
+subject = 'Message from ' + str(host_name)  # The subject line
 
 msg = MIMEMultipart()
 msg['From'] = email_sender
@@ -21,23 +20,32 @@ msg['Subject'] = subject
 
 
 def send_alarm(filename):
-    message = str(filename)+str("\n is dead!\n Please login and check!")
+    filename = "dfe"
+    message = str(filename) + str("\n\n is dead!\n Please login and check!\n")
+    # read the last 50 line
+    f = open(filename, "r")
+    lines = f.readlines()
+    for line in lines[-50:]:
+        message += line + str("\n")
+    message += "\nAbove is the last 50 lines\n"
+
     # Attach the message to the MIMEMultipart object
     msg.attach(MIMEText(message, 'plain'))
 
-    server = smtplib.SMTP('smtp.gmail.com', 587) # Connect to the server
-    server.starttls() # Use TLS
-    server.login(email_sender, password_sender) # Login to the email server
-    text = msg.as_string() # You now need to convert the MIMEMultipart object to a string to send
+    server = smtplib.SMTP('smtp.gmail.com', 587)  # Connect to the server
+    server.starttls()  # Use TLS
+    server.login(email_sender, password_sender)  # Login to the email server
+    text = msg.as_string()  # You now need to convert the MIMEMultipart object to a string to send
     server.sendmail(email_sender, email_receiver, text)
     server.quit()
 
+
 def main(filename):
-    while 1<3:
-        b1=os.path.getsize(filename)
+    while 1 < 3:
+        b1 = os.path.getsize(filename)
         time.sleep(60)
-        b2=os.path.getsize(filename)
-        if b1==b2:
+        b2 = os.path.getsize(filename)
+        if b1 == b2:
             send_alarm(filename)
             exit()
         else:
@@ -45,9 +53,9 @@ def main(filename):
 
 
 # main body
-file=sys.argv[1]
+file = sys.argv[1]
 if os.path.isfile(file):
-    print("Now the file\n"+str(file)+
+    print("Now the file\n" + str(file) +
           "\nis monitoring")
     main(file)
 else:
